54bad24c7fd5a60b43c593a341837a43e007edb7
helmutkian <helmut.rohrbacher@gmail.com>
2012-12-18 22:01:49 -0800

    Refactored thrush operators

--- thrush/thrush.lisp
+++ thrush/thrush.lisp
@@ -2,16 +2,18 @@
 ;(in-package :com.helmutkian.utils.thrush)
 
 
+
 (defmacro -> (value &body body)
   `(progn
-     ,(loop for form in (cons nil body)
-	    for ->form = value
-	      then (list* (first form) ->form (rest form))
-	    finally (return ->form))))
-
+     ,(reduce (lambda (inner outer)
+		(list* (car outer) inner (cdr outer)))
+	      body
+	      :initial-value value)))
+  
 (defmacro ->> (value &body body)
   `(progn
-     ,(loop for form in (cons nil body)
-	    for ->>form = value
-	      then (append form (list ->>form))
-	    finally (return ->>form))))
+     ,(reduce (lambda (inner outer)
+		(append outer (list inner)))
+	      body
+	      :initial-value value)))
+
