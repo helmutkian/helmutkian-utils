9d3349e39a73d9a035546e3fab55c25d6983f653
helmutkian <helmut.rohrbacher@gmail.com>
2013-01-22 19:37:30 -0800

    Reorganized. Moved ITERATORS and DATA-STRUCTS to their own projects.

--- iterator/bidirectional-iterator.lisp
+++
@@ -1,8 +0,0 @@
-
-(define-mixin-class bidirectional-iterator-mixin (iterator-mixin) ())
-
-(defgeneric iterator-prev (iterator)
-  (:documentation "Reverses the given external iterator to the previous element of the collection"))
-
-(defgeneric iterator-has-prev-p (iterator)
-  (:documentation "Determines whether the given external iterator has an element previous the current one."))
